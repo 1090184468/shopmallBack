bd97a6700dfb99f66b04b79c543b681a95fa223b
lbh117583010116 <1090184468@qq.com>
2020-06-01 12:53:38 +0800

    修改一下

--- ShopMallBack/src/com/bobo/util/JSONUtil.java
+++ ShopMallBack/src/com/bobo/util/JSONUtil.java
@@ -7,4 +7,8 @@ public class JSONUtil {
 		String str="{msg:'请求成功',request:";
 		return str+json+"}";
 	}
+	public static String returnJsonWithCode(Object json) {
+		String str="{code:200,msg:'请求成功',request:";
+		return str+json+"}";
+	}
 }
